3852e9c8842a9b5fac31e21cf0a86c18f7ff6987
amirathor <96159731+amirathor@users.noreply.github.com>
2025-02-05 18:27:34 +0530

    Update classoption.py

--- classoption.py
+++ classoption.py
@@ -18,13 +18,13 @@ class OptionChainData:
         self.url_indices = "https://www.nseindia.com/api/allIndices"
 
         # Headers
-        self.headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-            'Accept': 'application/json'
-        }
+        self.headers ={
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+}
 
         self.session = requests.Session()
-        request = self.session.get(self.url_oc, headers=self.headers, timeout=5)
+        request = self.session.get(self.url_oc, headers=self.headers, timeout=15)
         self.cookies = dict(request.cookies)
         # Set cookies
         self.session.cookies.update({'cookie_name': 'cookie_value'})
